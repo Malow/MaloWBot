c27a4ab2ab6d71a28ba44c075bf5e9d3ffa02d76
Chris <chriscgalbraith@gmail.com>
2018-12-08 15:33:19 +0000

    Added better logic for Rogues/warriors Warr: Casts Heroic Strike when has rage Rogues: cast SS
    Signed-off-by: Chris <chriscgalbraith@gmail.com>

--- src/Rogue.lua
+++ src/Rogue.lua
@@ -1,5 +1,10 @@
 function mb_Rogue(msg)
 	AssistByName(msg)
 	FollowByName(msg, true)
+	--mb_Print(GetSpellCooldown("Feint"))
+	CastSpellByName("attack")
+	--if PlayerHasSpells("Feint") and GetSpellCooldown("Feint") == 0 then
+	--	CastSpellByName("Feint")
+	--elseif GetSpellCooldown("Feint") > 0 then
 	CastSpellByName("Sinister Strike")
 end
--- src/Warrior.lua
+++ src/Warrior.lua
@@ -2,4 +2,5 @@ function mb_Warrior(msg)
 	AssistByName(msg)
 	FollowByName(msg, true)
 	CastSpellByName("Attack")
+	CastSpellByName("Heroic Strike")
 end

3c7b956f202c7f8b391fc01ec2b53fbe383158fc
MaloW <lowegrenmarcus@gmail.com>
2018-12-28 16:49:25 +0100

    Stop trading repair bots
    Signed-off-by: MaloW <lowegrenmarcus@gmail.com>

--- src/Functions.lua
+++ src/Functions.lua
@@ -166,6 +166,8 @@ function mb_IsIgnoredTradeItem(itemName)
 		return true
 	elseif itemName == "Gyromatic Micro-Adjustor" then
 		return true
+	elseif itemName == "Field Repair Bot 74A" then
+		return true
 	end
 	for i = max_GetTableSize(ITEMS_WATER), 1, -1 do
 		if itemName == ITEMS_WATER[i] then

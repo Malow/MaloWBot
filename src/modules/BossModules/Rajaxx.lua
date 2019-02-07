351f476ed972ee547a9272caee8216194dbe0f6b
MaloW <lowegrenmarcus@gmail.com>
2019-02-07 17:54:03 +0100

    Ignore dispelling Thunderclap on Rajaxx
    Signed-off-by: MaloW <lowegrenmarcus@gmail.com>

--- src/modules/BossModules/Rajaxx.lua
+++ src/modules/BossModules/Rajaxx.lua
@@ -1,10 +1,16 @@
+DEBUFF_TEXTURE_RAJAXX_THUNDERCLAP = "Interface\\Icons\\Spell_Nature_ThunderClap"
+
 function mb_BossModule_Rajaxx_Load()
     mb_currentBossModule.unloadFunction = mb_BossModule_Rajaxx_Unload
     mb_currentBossModule.hunterLogic = mb_BossModule_Rajaxx_HunterLogic
+    mb_currentBossModule.priestLogic = mb_BossModule_Rajaxx_PriestLogic
+    mb_currentBossModule.paladinLogic = mb_BossModule_Rajaxx_PaladinLogic
+    mb_shouldDispel = false
 end
 mb_RegisterBossModule("rajaxx", mb_BossModule_Rajaxx_Load)
 
 function mb_BossModule_Rajaxx_Unload()
+    mb_shouldDispel = true
 end
 
 function mb_BossModule_Rajaxx_HunterLogic()
@@ -15,6 +21,24 @@ function mb_BossModule_Rajaxx_HunterLogic()
         if max_CastSpellIfReady("Frost Trap") then
             return true
         end
+    end
+    return false
+end
+
+function mb_BossModule_Rajaxx_PriestLogic()
+    local unitFilter = UNIT_FILTER_DOES_NOT_HAVE_DEBUFF
+    unitFilter.debuff = DEBUFF_TEXTURE_RAJAXX_THUNDERCLAP
+    if mb_CleanseRaidMemberThrottled("Dispel Magic", "Magic", nil, nil, unitFilter, true) then
+        return true
+    end
+    return false
+end
+
+function mb_BossModule_Rajaxx_PaladinLogic()
+    local unitFilter = UNIT_FILTER_DOES_NOT_HAVE_DEBUFF
+    unitFilter.debuff = DEBUFF_TEXTURE_RAJAXX_THUNDERCLAP
+    if mb_CleanseRaidMemberThrottled("Cleanse", "Magic", "Poison", "Disease", unitFilter, true) then
+        return true
     end
     return false
 end

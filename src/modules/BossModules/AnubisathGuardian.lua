55c356746471f174f38464412785e496b9cba660
MaloW <lowegrenmarcus@gmail.com>
2019-02-07 13:44:49 +0100

    Small fix
    Signed-off-by: MaloW <lowegrenmarcus@gmail.com>

--- src/modules/BossModules/AnubisathGuardian.lua
+++ src/modules/BossModules/AnubisathGuardian.lua
@@ -1,6 +1,6 @@
 function mb_BossModule_AnubisathGuardian_Load()
     mb_currentBossModule.unloadFunction = mb_BossModule_AnubisathGuardian_Unload
-    mb_MoveOutModule_RegisterAutomaticFuckOffDebuffSpell("Plague Affect")
+    mb_MoveOutModule_RegisterAutomaticFuckOffDebuffSpell("Plague Effect")
 end
 mb_RegisterBossModule("anubisathGuardian", mb_BossModule_AnubisathGuardian_Load)
 
